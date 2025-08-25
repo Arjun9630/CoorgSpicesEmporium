ac690e3fbd28abbbffb0cabedcf835bdeae58193
Arjun.P <arjun.p9630@gmail.com>
2025-08-25 23:20:19 +0530

    AWS S3 Integration

--- coorgspices/settings.py
+++ coorgspices/settings.py
@@ -103,3 +103,11 @@ LOGIN_REDIRECT_URL = config("LOGIN_REDIRECT_URL", default="/")
 ACCOUNT_LOGOUT_REDIRECT_URL = config("ACCOUNT_LOGOUT_REDIRECT_URL", default="/")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AWS S3 Settings for Media Files
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
+AWS_QUERYSTRING_AUTH = False  # optional, public URLs
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
