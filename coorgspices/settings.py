edc8ef1c7dde042d1558ee1692953afcc09c116d
Arjun.P <arjun.p9630@gmail.com>
2025-08-26 02:09:53 +0530

    Settigs.py mod

--- coorgspices/settings.py
+++ coorgspices/settings.py
@@ -114,4 +114,4 @@ AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
 AWS_QUERYSTRING_AUTH = False  # optional, public URLs
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_DEFAULT_ACL = "public-read"
+AWS_DEFAULT_ACL = None
